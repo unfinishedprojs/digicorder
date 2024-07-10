43932742c17035c16761d2de99963e14e56f9961
144reasons <39058370+144reasons@users.noreply.github.com>
2024-07-11 00:35:47 +0200

    rewrite of the entire func v3

--- src/lib/makeVideo.ts
+++ src/lib/makeVideo.ts
@@ -10,11 +10,15 @@ function arrayBufferToUint8Array(buffer: ArrayBuffer): Uint8Array {
 
 export async function createVideo(imageBuffers: ArrayBuffer[]): Promise<string> {
     const ffmpeg = new FFmpeg();
+    
+    console.log('Loading ffmpeg')
 
     await ffmpeg.load({
         coreURL: await toBlobURL(`${baseURL}/ffmpeg-core.js`, 'text/javascript'),
         wasmURL: await toBlobURL(`${baseURL}/ffmpeg-core.wasm`, 'application/wasm'),
       });
+
+      console.log('loaded, loading images')
 
     for (let i = 0; i < imageBuffers.length; i++) {
         ffmpeg.writeFile(`image${i}.jpg`, new Uint8Array(imageBuffers[i]));
@@ -25,17 +29,25 @@ export async function createVideo(imageBuffers: ArrayBuffer[]): Promise<string> 
         inputString += `-i image${i}.jpg `;
     }
 
+    console.log('loaded. generating video')
+
     await ffmpeg.exec(['-framerate', '30', inputString, '-c:v', 'libx264', 'output.mp4']);
+
+    console.log('generated video. creating blob url')
 
     const data = await ffmpeg.readFile('output.mp4');
 
     const blob = new Blob([data.buffer], { type: 'video/mp4' });
     const url = URL.createObjectURL(blob);
 
+    console.log('done. cleaning up...')
+
     ffmpeg.unmount('output.mp4');
     for (let i = 0; i < imageBuffers.length; i++) {
         ffmpeg.unmount(`image${i}.jpg`);
     }
+
+    console.log('everything clean, returning url now...')
 
     return url;
 }
